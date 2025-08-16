2f924cbe924f252f8fd7224161a31b56ebb87c3b
Charlie85270 <34569321+Charlie85270@users.noreply.github.com>
2025-08-16 18:37:11 -0400

    create on all testnet chains  (#45)
    * fix build
    
    * create contracts on all chains in 1 tx

--- backend/src/chain.ts
+++ backend/src/chain.ts
@@ -1,5 +1,7 @@
 export enum ChainId {
-  BASE_SEPOLIA = 84532,
+  BASE_TESTNET = 84532,
+  ETHEREUM_TESTNET = 11155111,
+  ARBITRUM_TESTNET = 421614,
 }
 
 export type ChainInfo = {
@@ -22,6 +24,28 @@ export const chains: Record<number, ChainInfo> = {
     factoryContract: '0xc1959eBe4D66b153b4c8cB4FECe279517Ea6e375',
     domain: 6,
   },
+  11155111: {
+    chainId: 11155111,
+    chainName: 'Ethereum Sepolia',
+    chainCaiP: 'eip155:11155111',
+    feeControllerContract: '0x85b03e28Db7179431fcdD49030Af90b0001e3933',
+    receiverContract: '0x3aFB4C63DeD70Ed9feB962623Ff27937114e2365',
+    factoryContract: '0x7F487aC302218B185d28644d4678F6f454fc023e',
+    domain: 6,
+  },
+  421614: {
+    chainId: 421614,
+    chainName: 'Arbitrum Sepolia',
+    chainCaiP: 'eip155:421614',
+    feeControllerContract: '0x85b03e28Db7179431fcdD49030Af90b0001e3933',
+    receiverContract: '0x3aFB4C63DeD70Ed9feB962623Ff27937114e2365',
+    factoryContract: '0x7F487aC302218B185d28644d4678F6f454fc023e',
+    domain: 6,
+  },
 };
 
-export const CURRENT_CHAIN_ID = ChainId.BASE_SEPOLIA;
+export const CURRENT_CHAIN_IDS = [
+  ChainId.ARBITRUM_TESTNET,
+  ChainId.ETHEREUM_TESTNET,
+  ChainId.BASE_TESTNET,
+];
